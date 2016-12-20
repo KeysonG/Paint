9b68ed439fee7879d007a6a90648d3e9878d0335
KeysonG <gideon.keyson@gmail.com>
2016-12-20 19:50:19 +0200

    type first version

--- script.js
+++ script.js
@@ -1,55 +1,109 @@
 var colors = ["#333", "#fff", "red", "grey"];
 var clicked=0;
-var paint=false;
+var paint = false;
+// option 1 is to color
+// option 2 is to type
+var option = 0; 
+
+
 function generateColors(){
 	for(var i = 0; i<colors.length; i++){
+		// create menu
 		var colorpick = document.createElement("div");
 		var menu = document.getElementById("menu");
 		colorpick.style.background=colors[i];
 		colorpick.classList.add("colorpick");
 		colorpick.id = "s"+i;
-
 		menu.appendChild(colorpick);
+		
+		// drawing part
 		colorpick.addEventListener("click", selectedColor)
-
 		var canvas = document.getElementById("canvas");
-				canvas.addEventListener("mousedown", mouseDown);
+		canvas.addEventListener("mousedown", mouseDown);
 		canvas.addEventListener("mouseup", mouseUp);
-
-
 		canvas.addEventListener("mousemove", draw);
 	}
+	var textOption = document.createElement("div");
+	textOption.innerHTML= "T";
+	textOption.classList.add("textOption");
+	textOption.id = "text-option";
+	menu.appendChild(textOption);
+	textOption.addEventListener("click", clickType);
+	canvas.addEventListener("click", typeText);
+
+}
+function clickType(){
+			option=2;
+
+}
+function typeText(e){
+	if(option=2){
+	var canvas = document.getElementById("canvas");
+	var input = document.createElement("input");
+	input.className = "input";
+		input.id = "input";
+
+	var y=e.pageY;
+	var x=e.pageX;
+
+	input.style.top= y + "px";
+	input.style.left= x + "px";
+	canvas.appendChild(input);
+	input.focus();
+
+	input.addEventListener("change", addText);
+	}
+
+}
+function addText(){
+	console.log("changed");
+	var input = document.getElementById("input");
+	var text = document.createElement("div");
+	console.log(input.style.top);
+	text.style.top=input.style.top;
+	text.style.left= input.style.left;
+	canvas.appendChild(text);
+	text.innerText= input.value;
+	text.style.position= "absolute";
+	this.remove();
 }
 function selectedColor(e){
+	option=1;
 	clicked = e.target.id;
-	console.log(clicked);
+
 	e.target.classList.add("selected");
 
 }
 function mouseDown(){
+
 	paint= true;
+
 }
 function mouseUp(){
 	paint= false;
+	option=1;
 }
 function draw(e){
-	if(paint){
-	var point = document.createElement("div");
-	point.style.background = colors[clicked[1]];
-	console.log(point);
-	point.style.width = "5px";
-	point.style.height = "5px";
-	var canvas = document.getElementById("canvas");
-	point.classList.add("point");
-	var y=e.pageY;
-	var x=e.pageX;
-	console.log(y);
-	console.log(x);
-	point.style.top= y + "px";
-	point.style.left= x + "px";
-	canvas.appendChild(point);
+	if(paint && option===1){
+		var point = document.createElement("div");
+		point.style.background = colors[clicked[1]];
+
+		point.style.width = "5px";
+		point.style.height = "5px";
+		var canvas = document.getElementById("canvas");
+		point.classList.add("point");
+		var y=e.pageY;
+		var x=e.pageX;
+
+		point.style.top= y + "px";
+		point.style.left= x + "px";
+		canvas.appendChild(point);
+		type = false;
 	}
 } 
+
+
+
 function init(){
 	generateColors();
 }
